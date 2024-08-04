653a6c29da862d5d89e89ef803804515c3e7a085
laa66 <max.sclz94@gmail.com>
2024-08-04 17:20:58 +0200

    Init commit

---
+++ src/main/java/com/laa66/githubInspector/GithubInspectorApplication.java
@@ -0,0 +1,13 @@
+package com.laa66.githubInspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubInspectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laa66/githubInspector/GithubInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laa66.githubInspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubInspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
